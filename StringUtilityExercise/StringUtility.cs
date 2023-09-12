674656ed258781bb9557d6ef8b94dd80a65274bc
Evgeniy Gordienko <112853162+eudk@users.noreply.github.com>
2023-09-12 12:03:14 +0200

    Add project files.

---
+++ StringUtilityExercise/StringUtility.cs
@@ -0,0 +1,26 @@
+﻿namespace StringUtilityExercise
+{
+    public class StringUtility
+    {
+        public int CountOccurences(string strToCheck, string stringToFind)
+        {
+            strToCheck = strToCheck.ToLower(); // Insensitive str
+            var stringAsCharArray = strToCheck.ToCharArray();
+            var stringToCheckForAsChar = stringToFind.ToCharArray()[0];
+            var occuranceCount = 0;
+
+            // Character loop
+            for (var characterIndex = 0; characterIndex <= stringAsCharArray.GetUpperBound(0); characterIndex++)
+            {
+                if (stringAsCharArray[characterIndex] == stringToCheckForAsChar)
+                {
+                    occuranceCount++;
+                }
+            }
+
+            return occuranceCount;
+        }
+
+
+    }
+}
---
+++ StringUtilityExerciseTests/StringUtilityTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringUtilityExercise;
+
+namespace TestStringUtility
+{
+    [TestClass()]
+    public class StringUtilityTests
+    {
+        [TestMethod]
+        public void ShouldFindOneYInMysterious()
+        {
+            string stringToCheck = "mysterious";
+            string stringToFind = "y";
+            int expectedResult = 1;
+
+            StringUtility classUnderTest = new StringUtility();
+            int actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [TestMethod]
+        public void ShouldFindTwoSInMysterious()
+        {
+            string stringToCheck = "mysterious";
+            string stringToFind = "s";
+            int expectedResult = 2;
+
+            StringUtility classUnderTest = new StringUtility();
+            int actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+        [TestMethod]
+        public void SearchShouldBeCaseInsensitive()
+        {
+            string stringToCheck = "mySterious";
+            string stringToFind = "s";
+            int expectedResult = 2;
+
+            StringUtility classUnderTest = new StringUtility();
+            int actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
